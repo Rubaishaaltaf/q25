47263dc999e0ed30deb5adc0544664a21008d642
Rubaishaaltaf <160398911+Rubaishaaltaf@users.noreply.github.com>
2024-04-20 14:28:45 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// Define Variable
+var aliencolor = "White";
+if (aliencolor === "Black") {
+    // Code Block
+    console.log("Alien color is Black players just earned 5 points.");
+}
+if (aliencolor === "White") {
+    console.log("Alien Color is White");
+}
---
+++ main.ts
@@ -0,0 +1,11 @@
+// Define Variable
+let aliencolor = "White";
+
+if (aliencolor === "Black"){
+    // Code Block
+    console.log("Alien color is Black players just earned 5 points.");
+}
+
+if(aliencolor === "White"){
+    console.log("Alien Color is White");
+}
